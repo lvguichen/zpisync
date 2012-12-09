fbb6f9dbf5035a8183f899ad54afcf2c20feff8a
Marcin Wisnicki <mwisnicki@gmail.com>
2012-12-09 18:19:01 +0100

    Realtime file scanner.
    Change runtime from JavaSE-1.7 to unspecified to allow access to
    Sun-specific FILE_TREE event that is required for proper observation on
    Windows. Other systems are currently not supported.

--- zpisync-desktop/src/zpisync/desktop/FileScanner.java
+++ zpisync-desktop/src/zpisync/desktop/FileScanner.java
@@ -8,7 +8,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
 import java.nio.file.WatchEvent.Kind;
+import java.nio.file.WatchEvent.Modifier;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -38,6 +40,8 @@ public class FileScanner implements Closeable {
 	public static void main(String[] args) {
 		try (FileScanner scanner = new FileScanner(new File(Util.getHomeDir(), "Google Drive"))) {
 			scanner.scan();
+			scanner.startWatcher();
+			System.in.read();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -61,15 +65,85 @@ public class FileScanner implements Closeable {
 				return FileVisitResult.CONTINUE;
 			}
 		});
-
-		if (true)
-			return;
-		watchService = rootPath.getFileSystem().newWatchService();
-		WatchKey watchKey = rootPath.register(watchService, AllEvents);
 	}
+
+	private void startWatcher() throws IOException {
+		watchService = rootPath.getFileSystem().newWatchService();
+		// XXX FILE_TREE is specific to SunJDK+Windows but there is no other way
+		// to do this reliably
+		watchKey = rootPath.register(watchService, AllEvents, com.sun.nio.file.ExtendedWatchEventModifier.FILE_TREE);
+
+		watcherThread = new Thread(watcherLoop, "FS-Watcher");
+		watcherThread.setDaemon(true);
+		watcherThread.start();
+
+		log.fine("FileWatcher started");
+	}
+
+	private WatchKey watchKey;
+	private Thread watcherThread;
+	private Runnable watcherLoop = new Runnable() {
+
+		@Override
+		public void run() {
+			for (;;) {
+				// wait for key to be signaled
+				WatchKey key;
+				try {
+					key = watchService.take();
+				} catch (InterruptedException x) {
+					return;
+				}
+
+				for (WatchEvent<?> event : key.pollEvents()) {
+					WatchEvent.Kind<?> kind = event.kind();
+					// TBD - provide example of how OVERFLOW event is handled
+					if (kind == StandardWatchEventKinds.OVERFLOW) {
+						log.severe("Unhandled file watcher overflow");
+						continue;
+					}
+					publish(createChangeEvent((WatchEvent<Path>) event, key));
+				}
+
+				// reset key return if directory no longer accessible
+				boolean valid = key.reset();
+				if (!valid) {
+					break;
+				}
+			}
+		}
+
+		private void publish(Object createChangeEvent) {
+			// TODO Auto-generated method stub
+
+		}
+
+		private Object createChangeEvent(WatchEvent<Path> event, WatchKey key) {
+			// TODO Auto-generated method stub
+			System.out.printf("File changed: %s %s %d\n", event.kind(), event.context(), event.count());
+			return null;
+		}
+	};
 
 	@Override
 	public void close() throws IOException {
 		// TODO stop watcher
+		if (watcherThread != null) {
+			watcherThread.interrupt();
+			try {
+				watcherThread.join(2000);
+				log.info("FileWatcher stopped");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			watcherThread = null;
+		}
+
+		if (watchKey != null) {
+			watchKey.cancel();
+			log.info("FileWatcher unregistered");
+			watchKey = null;
+		}
 	}
 }
